1ab9fd93038ebac6a284f4246c1d9f72fe04f618
Данил Неверов <xxdenkenxx@gmail.com>
2023-12-02 20:39:49 +0300

    Добавил миграции

---
+++ database/migrations/2023_12_02_173815_create_contacts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('address');
+            $table->string('mail');
+            $table->string('phone');
+            $table->string('work_hours');
+            $table->string('shipping_hours');
+            $table->string('map_x');
+            $table->string('map_y');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('contacts');
+    }
+};
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -3,11 +3,11 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        @yield('seo')
         <link rel="stylesheet" href="{{ asset('fonts/fonts.css') }}" media="screen">
         <link rel="stylesheet" href="{{ asset('css/app.css') }}" media="screen">
         <link rel="stylesheet" href="{{ asset('css/headerAndFooter.css') }}" media="screen">
         <link rel="stylesheet" href="{{ asset('css/normalize.css') }}" media="screen">
+        @yield('seo')
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css"/>
         <script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
         <script src="https://code.jquery.com/jquery-3.7.1.slim.min.js" integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8=" crossorigin="anonymous"></script>
